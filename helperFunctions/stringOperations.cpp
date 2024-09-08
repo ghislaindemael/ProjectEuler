d2183f1ee4db3a14f5983a505e35ce6ee510ee0b
Ghislain Demaël <demaelghislain@gmail.com>
2024-09-08 13:18:10 +0200

    Created stringOperations.h/cpp

---
+++ helperFunctions/stringOperations.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ghisl on 08/09/2024.
+//
+
+#include "stringOperations.h"
+
+bool isUniquelyPandigital(const std::string& input) {
+    if (input.length() != 9) {
+        return false;
+    }
+
+    std::set<char> seenDigits;
+
+    for (char c : input) {
+        if (c < '1' || c > '9') {
+            return false;
+        }
+        if (seenDigits.contains(c)) {
+            return false;
+        }
+        seenDigits.insert(c);
+    }
+
+    return true;
+}
---
+++ helperFunctions/stringOperations.h
@@ -0,0 +1,12 @@
+//
+// Created by ghisl on 08/09/2024.
+//
+
+#ifndef STRINGOPERATIONS_H
+#define STRINGOPERATIONS_H
+#include <set>
+#include <string>
+
+bool isUniquelyPandigital(const std::string& input);
+
+#endif //STRINGOPERATIONS_H
