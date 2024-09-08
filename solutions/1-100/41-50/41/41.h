1cae2aa11ee0a8effa3475c76185fe8179e6788e
Ghislain Demaël <demaelghislain@gmail.com>
2024-09-08 18:30:37 +0200

    Problem 41: Pandigital Prime

---
+++ solutions/1-100/41-50/41/41.h
@@ -0,0 +1,15 @@
+//
+// Created by ghisl on 08/09/2024.
+//
+
+#ifndef INC_41_H
+#define INC_41_H
+#include <string>
+#include <vector>
+
+#include "helperFunctions/primeFunctions.h"
+#include "helperFunctions/stringOperations.h"
+
+std::string getLargestPandigitalPrimeUsingSegmentedSieve(int limit);
+
+#endif //INC_41_H
